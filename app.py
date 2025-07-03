b98d13e5cfe6b218dc346ce9e490b265f0f57725
muruk511 <tontanmotors@gmail.com>
2025-07-03 21:31:39 +1000

    Update app.py

--- app.py
+++ app.py
@@ -80,5 +80,5 @@ def ping():
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 5000))  # Use PORT env var or default to 5000
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
