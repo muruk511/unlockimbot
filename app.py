974659d3e3d54e668756a7488416e95d9640ae44
muruk511 <tontanmotors@gmail.com>
2025-07-03 21:19:52 +1000

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 app = Flask(__name__)
 
 # Initialize Firebase
-cred_path = os.getenv("FIREBASE_CREDENTIALS", "unlockimbot-bffb5-firebase-adminsdk-fbsvc-5293abaa2b.json")
+cred_path = os.getenv("FIREBASE_CREDENTIALS", "unlockimbot-bffb5-firebase-adminsdk-fbsvc-3939329081.json")
 cred = credentials.Certificate(cred_path)
 initialize_app(cred)
 db = firestore.client()
